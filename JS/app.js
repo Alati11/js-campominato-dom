e1772ee5ba07f47bbb7e7b83f11fbb3bdd3b2f9f
Alati11 <peppealati@gmail.com>
2023-10-17 01:45:18 +0200

    error control bombs

--- JS/app.js
+++ JS/app.js
@@ -2,16 +2,16 @@
 const gridElement = document.querySelector('.grid');
 console.log(gridElement);
 
-const playBtnElement = document.getElementById('play-btn')
-console.log(playBtnElement)
+const playBtnElement = document.getElementById('play-btn');
+console.log(playBtnElement);
 
-
+let score = 0;
 
 playBtnElement.addEventListener('click', function () {
     // svuota la griglia dalle celle generate in precedenza
     gridElement.innerHTML = ''
     const bombe = getArrayOfRandomIntBetween(1, 100, 16);
-    console.log(bombe);
+    // console.log(bombe);
 
     let boxListHTML = '';
     for (let i = 0; i < 100; i++) {
@@ -30,8 +30,6 @@ playBtnElement.addEventListener('click', function () {
     const cellDomElements = document.querySelectorAll('.box');
     //console.log(cellDomElements);   
 
-  
-
     // ciclando l'array con le caselle del dom
     for (let i = 0; i < cellDomElements.length; i++) {
         const currentCellElement = cellDomElements[i];
@@ -39,16 +37,18 @@ playBtnElement.addEventListener('click', function () {
 
         // ad ogni casella aggiungo l'event listener
         currentCellElement.addEventListener('click', function () {
-            currentCellElement.classList.add('bg-green');
-            console.log(currentCellElement);
+            currentCellElement.classList.add('bg-blue');
+            // console.log(currentCellElement);
 
-            if (bombsArray.includes(n) === true) {
+            // controllare se il numero cliccato è presente nella lista dei numeri generati
+            if(bombsArray.includes(n) == true) {              
                 currentCellElement.classList.add('bg-red');
-                console.log( "Hai perso!!!");
-                } else { currentCellElement = currentCellElement + i;
-            
-            
-                }
+                console.log("Hai perso!!! Partita Finita!")
+                document.getElementById('message').innerHTML = "Hai perso! Partita finita!"
+            } else {
+                score++;
+            }
+        
         })
     }
 })
@@ -56,9 +56,8 @@ playBtnElement.addEventListener('click', function () {
 function getArrayOfRandomIntBetween(minRange, maxRange, number) {
     const bombsArray = []
 
-    // scrivere dentro  l'array 16 numeri random non duplicati
+    // scrivere dentro l'array 16 numeri random non duplicati
     while (bombsArray.length < number) {
-
         // generare un numero random da rangeMin a rangeMAx
         const n = getRandomIntInclusive(minRange, maxRange);
         console.log(n);
@@ -68,20 +67,17 @@ function getArrayOfRandomIntBetween(minRange, maxRange, number) {
         if (bombsArray.includes(n) === false) {
             // pushare il numero nell'array di bombe
             bombsArray.push(n);
-            console.log(bombsArray);
+            // console.log(bombsArray);   
         }
+     
     }
-
-    // titolo.addEventListener('click', onCellClick);
-    // return array di numeri generati
-    return bombsArray;
-}
-// controllare se il numero cliccato è presente nella lista dei numeri generati
-
-
-// paragrafo.addEventListener('click', onCellClick)    
+    
+ // return array di numeri generati
+ return bombsArray; 
+document.getElementById("score").innerHTML = "Hai totalizzato un punteggio di " + score;
+   
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+}}
